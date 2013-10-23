4cf5a83cc8f34b81e762f0a8d5ea2a5065d509c4
ssgariglia <ssgariglia@gmail.com>
2013-10-23 16:05:58 +0200

    Solución para PasterRico

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+  public static void main(String[] args) {
+    PastelRicoSolver solver = new PastelRicoSolver();
+
+    solver.solve();
+    System.out.println(solver.getSolutionCount());
+  }
+}
