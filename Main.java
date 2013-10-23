b025243fc5d43ac9c6a70406f31b53cd145d64f0
ssgariglia <ssgariglia@gmail.com>
2013-10-23 16:07:34 +0200

    Revert "Solución para PasterRico"
    This reverts commit 4cf5a83cc8f34b81e762f0a8d5ea2a5065d509c4.

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-  public static void main(String[] args) {
-    PastelRicoSolver solver = new PastelRicoSolver();
-
-    solver.solve();
-    System.out.println(solver.getSolutionCount());
-  }
-}
